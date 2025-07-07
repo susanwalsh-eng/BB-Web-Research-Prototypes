d268368574f805730fd70b04c9b098fcdac71b03
susanwalsh-eng <susanwalsh@monzo.com>
2025-07-07 11:20:18 +0100

    Update prototypes with latest code and correct GitHub Pages configuration

--- prototype-1/_next/static/chunks/app/layout-127fc929cd0c0517.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{347:()=>{},8346:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_f4b4ce"}},9387:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,8346,23)),Promise.resolve().then(l.t.bind(l,347,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[838,441,684,358],()=>s(9387)),_N_E=e.O()}]);
