4dce61ee9f164e9e91204d666e9634ab49588b09
susanwalsh-eng <susanwalsh@monzo.com>
2025-07-07 14:48:14 +0100

    Add working prototype builds with proper directory structure
    - Fixed CSS and JavaScript loading issues by creating proper basePath structure
    - Both prototype-1 and prototype-2 now serve correctly with all assets loading
    - Directory structure matches Next.js basePath configuration
    - Both prototypes accessible via single HTTP server on port 3000

---
+++ BB-Web-Research-Prototypes/July2025/build/prototype-1/_next/static/chunks/app/layout-bd533d17ab1eae97.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{347:()=>{},6251:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,8346,23)),Promise.resolve().then(l.t.bind(l,347,23))},8346:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_fc671b"}}},e=>{var s=s=>e(e.s=s);e.O(0,[191,690,441,684,358],()=>s(6251)),_N_E=e.O()}]);
---
+++ BB-Web-Research-Prototypes/July2025/build/prototype-2/_next/static/chunks/app/layout-27ce8e8f3df8a6c7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{347:()=>{},1639:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,8346,23)),Promise.resolve().then(l.t.bind(l,347,23))},8346:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_60c914"}}},e=>{var s=s=>e(e.s=s);e.O(0,[191,690,441,684,358],()=>s(1639)),_N_E=e.O()}]);
